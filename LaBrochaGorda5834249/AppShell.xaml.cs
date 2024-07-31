005b95929a47e193d311cc1f775c73accdf3c27b
emelyyy03 <emelyyy03@outlook.com>
2024-07-31 16:07:08 -0600

    Agregar archivos de proyecto.

---
+++ LaBrochaGorda5834249/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LaBrochaGorda5834249
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LaBrochaGorda5834249/PinturasModelo.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaBrochaGorda5834249
+{
+    public partial class PinturasModelo : ObservableObject
+    {
+        [ObservableProperty]
+        public string? precioMetro;
+
+        [ObservableProperty]
+        public string? metrosCuadrados;
+
+        [ObservableProperty]
+        public string? pago;
+
+        [RelayCommand]
+        private void ActualizarDatos()
+        {
+            if(double.TryParse(PrecioMetro, out double precio) && double.TryParse(MetrosCuadrados, out double metros))
+            {
+                Pago = (precio * metros).ToString();
+            }
+            else
+            {
+                Pago = "Digite valores numericos";
+            }
+        }
+    }
+}
